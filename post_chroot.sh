4d5a3851058fafe5527ad805cfca9a7dc92fe6d5
Lukas Does Dev <51633285+LukasDoesDev@users.noreply.github.com>
2021-07-31 19:51:12 +0300

    Update post_chroot.sh

--- post_chroot.sh
+++ post_chroot.sh
@@ -8,11 +8,13 @@ MAGENTA='\033[1;35m'
 CYAN='\033[1;96m'
 BLUE='\033[1;34m'
 
+if [[ `id -u` -eq 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+
 while true; do
   printf ${WHITE}"### Setting timezone\n"
-  printf ${CYAN}"Enter your timezone in this format: Region/City\n(ex, Europe/Helsinki, America/New_York, Asia/Singapore, Australia/Sydney)\n(https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)\n>"
+  printf ${CYAN}"Enter your timezone in this format: Area/City\n(ex, Europe/Helsinki, America/New_York, Asia/Singapore, Australia/Sydney)\n(https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)\n>"
   read timezone
-
+  
   ln -sf /usr/share/zoneinfo/${timezone} /etc/localtime
   
   if [ "$?" = "0" ]; then
@@ -25,17 +27,21 @@ done
 
 hwclock --systohc
 
-printf ${WHITE}"### Setting locales, keyboard etc\n"
-printf ${CYAN}"Enter your language in this form: \"<locale> <charset>\" where <locale> is one of the locales placed in /usr/share/i18n/locales and <charset> is one of the charsets placed in /usr/share/i18n/charmaps (ex, \"en_US UTF-8\")\n>"
-read lang1
+printf ${WHITE}"### Setting locale, charset and keymap\n"
 
-printf ${CYAN}"Enter your language in this form: \"<locale>\" where <locale> is one of the locales placed in /usr/share/i18n/locales (ex, \"en_US\")\n>"
-read lang2
+printf ${CYAN}"What locale do you want to use? Must be one of the locales placed in ${BLUE}/usr/share/i18n/locales${CYAN}. Example: fi_FI, en_US, en_GB, en_AU, de_DE. Remember that you can always change this later in ${BLUE}/etc/locale.conf${CYAN}\n>"
+read locale
 
-echo LANG=${lang2} > /etc/locale.conf
-echo "${lang1}" >> /etc/locale.gen
+printf ${CYAN}"What charset do you want to use? Must be one of the charsets placed in ${BLUE}/usr/share/i18n/charmaps${CYAN} (Ignore the .gz extension). Example: UTF-8, ISO-8859-1, MAC-UK, MACINTOSH. Remember that you can always change this later in ${BLUE}/etc/locale.conf${CYAN}\n>"
+read charset
 
-echo KEYMAP=fi > /etc/vconsole.conf
+printf ${CYAN}"What keymap do you want to use? Must be one of the keymaps placed in ${BLUE}/usr/share/kbd/keymaps${CYAN} (Ignore the .map.gz extension). Example: fi, us, pc110, jp106.\n>"
+read keymap
+
+echo LANG="${locale}.${charset}" > /etc/locale.conf
+echo "${locale}.${charset} ${charset}" >> /etc/locale.gen
+
+echo KEYMAP=${keymap} > /etc/vconsole.conf
 
 locale-gen
 
@@ -44,7 +50,7 @@ read newHostname
 echo $newHostname > /etc/hostname
 echo "127.0.0.1       localhost" >> /etc/hosts
 echo "::1             localhost" >> /etc/hosts
-echo "127.0.1.1       $newHostname.localdomain $newHostname" >> /etc/hosts
+echo "127.0.0.1       $newHostname.localdomain $newHostname" >> /etc/hosts
 
 
 
@@ -53,15 +59,15 @@ pacman --noconfirm -Sy networkmanager
 systemctl enable NetworkManager
 
 while true; do
-    printf ${LIGHTGREEN}"enter the password for your root user\n>"
+    printf ${LIGHTGREEN}"Enter the password for the root user\n>"
     read -s password
-    printf ${LIGHTGREEN}"re-enter the password for your root user\n>"
+    printf ${LIGHTGREEN}"Re-enter the password for the root user\n>"
     read -s password_compare
     if [ "$password" = "$password_compare" ]; then
-    echo "root:$password" | chpasswd
+        echo "root:$password" | chpasswd
         break
     else
-        printf ${LIGHTRED}"Passwords do not match, re enter them"
+        printf ${LIGHTRED}"Passwords do not match, re-enter them"
         printf ${WHITE}".\n"
         sleep 3
         clear
